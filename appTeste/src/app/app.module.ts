c13c68d26439071630ef30ba172fb9a1d74fb339
EzioCardosoJunior <ejcj1980@msn.com>
2023-03-27 15:24:50 -0300

    ttt

--- appTeste/src/app/app.module.ts
+++ appTeste/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { SidebarComponent } from './componentes/sidebar/sidebar.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
