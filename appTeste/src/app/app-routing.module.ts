8e35e31bfbbb597af61c92945adc61423fbd945f
EzioCardosoJunior <ejcj1980@msn.com>
2023-03-27 21:23:51 -0300

    login

--- appTeste/src/app/app-routing.module.ts
+++ appTeste/src/app/app-routing.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-
+import { LoginComponent } from './componentes/login/login.component';
 const routes: Routes = [
   {
     path:"app-home",
     component: HomeComponent
   },
+  {
+    path:"app-login",
+    component: LoginComponent
+  },
 
   {
     path: '', 
-    redirectTo: 'app-home', 
+    redirectTo: 'app-login', 
     pathMatch: 'full'
   },
 
--- appTeste/src/app/app.module.ts
+++ appTeste/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeaderComponent } from './componentes/header/header.component';
 import { SidebarComponent } from './componentes/sidebar/sidebar.component';
 import { FooterComponent } from './componentes/footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { ContentComponent } from './componentes/content/content.component';
+import { LoginComponent } from './componentes/login/login.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { HomeComponent } from './home/home.component';
     HeaderComponent,
     SidebarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    ContentComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
